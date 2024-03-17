a0904fcabe7134e1e358ef9274f8f854b7d925a8
jonaf <jony6484@gmail.com>
2024-03-17 15:06:54 +0000

    .

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -8,25 +8,16 @@ from pathlib import Path
 import torch
 from tqdm import tqdm
 
-
-
-
-def group_file_by_speaker(csv_file, src_folder, dest_folder):
-    speakers = pd.read_csv(csv_file, chunksize=10)
-    for chunk in speakers:
 import glob
 from collections import Counter
 
 import pandas as pd
-import shutil
-import os
+# import shutil
 from tqdm import tqdm
-
-import os
-import shutil
+# import shutil
 from datetime import datetime
 import logging
-import colorlog
+# import colorlog
 
 logger = logging.getLogger(__name__)
 
@@ -122,20 +113,29 @@ def get_files_in_folder(folder_path):
     files = glob.glob(folder_path + '/*')
     return files
 
-def group_file_by_speaker(csv_file, src_folder, dest_folder):
+
+def group_file_by_column(csv_file, src_folder, dest_folder, column='speaker', max_per_val=100):
+    val_counter = Counter()
     logger.info("start")
     speakers = pd.read_csv(csv_file, chunksize=1000)
     for chunk in tqdm(speakers):
         records = chunk.to_dict('records')
         for record in records:
-            speaker_folder = f'{dest_folder}/{record.get("speaker")}'
+            column_val = record.get(column)
+            val_counter.update([column_val])
+            column_val_count = val_counter.get(column_val)
+            if column_val_count > max_per_val:
+                continue
+            speaker_folder = f'{dest_folder}/{column_val}'
             if not os.path.exists(speaker_folder):
                 os.makedirs(speaker_folder)
             file_name = record.get("file")
             src_file = f'{src_folder}/{file_name}'
             dest_file = f'{speaker_folder}/{file_name}'
-            shutil.copyfile(src_file, dest_file)
 
+            shutil.copyfile(src_file, dest_file)
+    logger.info("end")
+    
     # for record in records:
 
 def pre_process_files(src_folder, dest_folder):
@@ -158,13 +158,12 @@ def pre_process_files(src_folder, dest_folder):
 
 
 def main():
-    src_folder = C.RAW_DATA_DIR
-    dest_folder = C.CLN_DATA_DIR 
-    pre_process_files(src_folder, dest_folder)
-    csv_file = './data/subset/hackathon_train_subset.csv'
-    src_folder = dest_folder
-    dest_folder = './data/subset/by_speaker'
-    group_file_by_speaker(csv_file, src_folder, dest_folder)
+    # pre_process_files(src_folder, dest_folder)
+    # csv_file = './data/subset/hackathon_train_subset.csv'
+    src_folder = C.CLN_DATA_DIR 
+    dest_folder = C.BY_SPEAKER_DIR
+    csv_file = C.CSV_DATA_FILE
+    group_file_by_column(csv_file, src_folder, dest_folder, max_per_val=20)
 
 
         
@@ -174,74 +173,6 @@ def main():
 
 
 if __name__ == '__main__':
-
     main()
 
-
-
-
-            shutil.copyfile(src_file, dest_file)
-    logger.info("end")
-
-def group_file_by_column(csv_file, src_folder, dest_folder, column='speaker', max_per_val= 100):
-    val_counter = Counter()
-    logger.info("start")
-    speakers = pd.read_csv(csv_file, chunksize=1000)
-    for chunk in tqdm(speakers):
-        records = chunk.to_dict('records')
-        for record in records:
-            column_val = record.get(column)
-            val_counter.update([column_val])
-            column_val_count = val_counter.get(column_val)
-            if column_val_count > max_per_val:
-                continue
-            speaker_folder = f'{dest_folder}/{column_val}'
-            if not os.path.exists(speaker_folder):
-                os.makedirs(speaker_folder)
-            file_name = record.get("file")
-            src_file = f'{src_folder}/{file_name}'
-            dest_file = f'{speaker_folder}/{file_name}'
-
-            shutil.copyfile(src_file, dest_file)
-    logger.info("end")
-
-def copy_files(csv_file, src_folder, dest_folder, file_column='file'):
-    logger.info("start")
-    files_df = pd.read_csv(csv_file, chunksize=1000)
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
-
-    for chunk in tqdm(files_df):
-        file_names = chunk[file_column].to_list()
-
-        for file_name in file_names:
-
-            src_file = f'{src_folder}/{file_name}'
-            dest_file =  f'{dest_folder}/{file_name}'
-            shutil.copyfile(src_file, dest_file)
-    logger.info("end")
-
-
-def download_from_gdrive(url, output):
-    gdown.download(url, output, quiet=False)
-
-    # for record in records:
-
-if __name__ == '__main__':
-    import gdown
-    # Google Drive URL of the file
-    url = 'https://drive.google.com/file/d/10mr9tLip_QhV0cEfp2FsdWoAvuPoscc9/view?usp=drive_link'
-    YOUR_FILE_ID = '10mr9tLip_QhV0cEfp2FsdWoAvuPoscc9'
-    YOUR_FILE_ID = '1EsBdxcycc7LjcIHCb1LzFnJxX5lGgIc4'
-    url = f'https://drive.google.com/uc?id={YOUR_FILE_ID}'
-    # Path to save the downloaded file
-    output = 'Speakathon.ipynb'
-    # Download the file
-    gdown.download(url, output, quiet=False)
-
-# wav_folder = '/home/avrash/ai/data/wav_files_subset'
-# dest_folder = '/home/avrash/ai/data/by_speaker'
-# csv_file_1 = '/home/avrash/ai/data/hackathon_train_subset.csv'
-
-# group_file_by_speaker(csv_file_1, wav_folder, dest_folder)
 
