05d1fbc7d6f508c01a552715592361a3e2a4423a
avrash <avrash@rafael.co.il>
2024-03-17 15:19:40 +0200

    copy files by columns

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.utils.file_utils import group_file_by_column
+from src.utils.file_utils import group_file_by_column, copy_files
 
 
 class MyTestCase(unittest.TestCase):
@@ -11,6 +11,13 @@ class MyTestCase(unittest.TestCase):
         dest_folder = '/home/avrash/ai/hakol-1/output'
         column = 'noise_type'
         group_file_by_column(csv_file, src_folder, dest_folder, column)
+
+    def test_copy_file(self):
+        csv_file = '/home/avrash/ai/hakol-1/data/subset/hackathon_train_subset.csv'
+        src_folder = '/home/avrash/ai/hakol-1/data/subset/wav_files_subset'
+        dest_folder = '/home/avrash/ai/hakol-1/output'
+        column = 'file'
+        copy_files(csv_file, src_folder, dest_folder, column)
 
 
 
