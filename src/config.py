33a1bc9dc4a406f8c8e6c3c9ff4f0e12db4024a5
jonaf <jony6484@gmail.com>
2024-03-17 14:12:10 +0000

    config

--- src/config.py
+++ src/config.py
@@ -2,9 +2,12 @@ from pathlib import Path
 
 
 ROOT = Path.cwd()
-
-DATA_DIR = ROOT / 'data'
-RAW_DATA_DIR = DATA_DIR / 'subset' / 'wav_files_subset'
-CLN_DATA_DIR = DATA_DIR / 'subset' / 'wav_files_subset_cln'
-
+DATA_DIR = Path('/workdir/data')
+RAW_DATA_DIR = DATA_DIR / 'wav_files'
+CLN_DATA_DIR = DATA_DIR / 'wav_files_cln'
+BY_SPEAKER_DIR = DATA_DIR / 'by_speaker'
+CSV_DATA_FILE = DATA_DIR / "hackathon_train.csv"
 Path(CLN_DATA_DIR).mkdir(parents=True, exist_ok=True)
+Path(BY_SPEAKER_DIR).mkdir(parents=True, exist_ok=True)
+
+SAMPLE_RATE = 16000
